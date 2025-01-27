e494e6a6bf85df21ba88133026360740edbefa71
rome-pro <robert.mende@audi.de>
2025-01-27 12:15:04 +0000

    Skript funktioniert

--- src/02_02/Program.cs
+++ src/02_02/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello C#!");
+﻿//meine erste Variable
+int meine_erste_Variable = 11;
+//meine zweite Variable
+int meine_zweite_Variable = 10;
+//mein Ergebnis
+int mein_Ergebnis = meine_erste_Variable + meine_zweite_Variable;
+Console.WriteLine(mein_Ergebnis);
